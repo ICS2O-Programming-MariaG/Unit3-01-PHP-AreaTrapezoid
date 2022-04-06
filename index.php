7e7ea5557767e6113d74a9c00bc0ea7fbf59d8b9
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-04-06 15:05:26 +0000

    Finished styling in CSS

--- index.php
+++ index.php
@@ -13,25 +13,31 @@
     <title>Calculating the Area of a Trapezoid in PHP - Maria Goemans</title>
   </head>
   <body>
-    <?php
-      echo "<h1>Calculating the Area of a Trapezoid in PHP with User Input</h1>";
-      echo "<p>Welcome! This web page will take your input and use the dimensions you choose to calculate the area of a trapezoid. See the image below for the calculations we will be using.</p>";
-    ?>
-    <img src="./images/formulas.png">
-    <?php
-      echo "<h3>Your Input</h3>";
-      echo "<p>Please fill in the form below to set the height and the lengths of sides a and b for a trapezoid. We will then calculate the area of the shape for you!</p>";
-    ?>
-    <!-- Form for user input -->
-    <form action="./calculations.php" method="post" target="display-result">
-      <label for="height">Height of the trapezoid</label>
-      <input type="float" name="height" placeholder="Height..."><br><br>
-      <label for="side-a">Length of side A</label>
-      <input type="float" name="side-a" placeholder="Side A length..."><br><br>
-      <label for="side-b">Length of side B</label>
-      <input type="float" name="side-b" placeholder="Side B length..."><br><br>
-      <input type="submit" value="Calculate">
-    </form>
+    <?php echo "<h1>Calculating the Area of a Trapezoid in PHP with User Input</h1>"; ?>
+    <!-- First div for style -->
+    <div id="div-one">
+      <?php
+        echo "<p>Welcome! This web page will take your input and use the dimensions you choose to calculate the area of a trapezoid. See the image below for the calculations we will be using.</p>";
+      ?>
+      <img src="./images/formulas.png">
+    </div>
+    <!-- Second div for style -->
+    <div id="div-two">
+      <?php
+        echo "<h3>Your Input</h3>";
+        echo "<p>Please fill in the form below to set the height and the lengths of sides a and b for a trapezoid. We will then calculate the area of the shape for you!</p>";
+      ?>
+      <!-- Form for user input -->
+      <form action="./calculations.php" method="post" target="display-result">
+        <label for="height">Height of the trapezoid</label>
+        <input type="float" name="height" placeholder="Height..."><br><br>
+        <label for="side-a">Length of side A</label>
+        <input type="float" name="side-a" placeholder="Side A length..."><br><br>
+        <label for="side-b">Length of side B</label>
+        <input type="float" name="side-b" placeholder="Side B length..."><br><br>
+        <input type="submit" value="Calculate">
+      </form>
+    </div>
     <!-- Iframe to display results -->
     <iframe id="display-result" name="display-result"></iframe>
   </body>
