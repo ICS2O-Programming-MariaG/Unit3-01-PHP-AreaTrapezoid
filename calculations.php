47ee386f1faeafed62c9225ff8770ef7136ef57f
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-04-06 20:45:42 +0000

    Rounded calculation to two decimal places

--- calculations.php
+++ calculations.php
@@ -3,7 +3,7 @@
   $sideA = floatval($_POST["side-a"]);
   $sideB = floatval($_POST["side-b"]);
   $sumOfSides = $sideA + $sideB;
-  $area = $sumOfSides / 2 * $height;
+  $area = number_format($sumOfSides / 2 * $height, 2);
 ?>
 
 The area of the trapezoid is <?php echo "$area"; ?> cm<sup>2</sup>.
